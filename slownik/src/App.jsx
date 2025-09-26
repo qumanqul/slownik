0d2048bc8a459c2e34b94e34a189f8c0c9ae7545
Savely Zalessky <savely.zalessky@gmail.com>
2025-09-26 10:19:17 +0200

    Fix admin page

--- slownik/src/App.jsx
+++ slownik/src/App.jsx
@@ -3,22 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Main from "./pages/Main.jsx";
 import Writers from "./pages/Writers.jsx";
-import WriterDetail from "./pages/WriterDetail.jsx";
-import { AccessibilityProvider } from "./contexts/AccessibilityContext";
 import Admin from "./pages/Admin.jsx";
+import WriterDetail from "./pages/WriterDetail.jsx";
 
 function App() {
   return (
-    <AccessibilityProvider>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/writers" element={<Writers />} />
-        <Route path="/writers/:id" element={<WriterDetail />} />
         <Route path="*" element={<h2>404 — Nie znaleziono strony</h2>} />
+        <Route path="/writers/:id" element={<WriterDetail />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </AccessibilityProvider>
+    </>
   );
 }
 
