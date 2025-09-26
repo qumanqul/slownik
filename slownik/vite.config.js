7cef30a6de186bf9b197716c005d7aa81f7c2133
qumanqul <defople@gmail.com>
2025-09-26 14:30:54 +0200

    outDir

--- slownik/vite.config.js
+++ slownik/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   preview: {
     host: "0.0.0.0",
     port: process.env.PORT || 4173,
