ec19829188e06572428537e0edbb0828aa154bd9
qumanqul <defople@gmail.com>
2025-09-26 14:26:21 +0200

    render

--- slownik/vite.config.js
+++ slownik/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: ["slownik.onrender.com"],
+  },
+});
