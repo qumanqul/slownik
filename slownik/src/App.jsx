408e47982a10680b20f2712d2a3f239060cc2354
qumanqul <defople@gmail.com>
2025-09-26 13:53:44 +0200

    0.5

--- slownik/src/App.jsx
+++ slownik/src/App.jsx
@@ -5,18 +5,21 @@ import Main from "./pages/Main.jsx";
 import Writers from "./pages/Writers.jsx";
 import Admin from "./pages/Admin.jsx";
 import WriterDetail from "./pages/WriterDetail.jsx";
+import { AccessibilityProvider } from "./contexts/AccessibilityContext.jsx";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/writers" element={<Writers />} />
-        <Route path="*" element={<h2>404 — Nie znaleziono strony</h2>} />
-        <Route path="/writers/:id" element={<WriterDetail />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <AccessibilityProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/writers" element={<Writers />} />
+          <Route path="*" element={<h2>404 — Nie znaleziono strony</h2>} />
+          <Route path="/writers/:id" element={<WriterDetail />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </AccessibilityProvider>
     </>
   );
 }
